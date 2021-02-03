c1be8ad3f7dd8caf38fd24f494eabe7ed4215325
fyardlest <fyardlest1@gmail.com>
2021-02-03 16:10:30 -0500

    Flex-box exercice

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,24 +3,56 @@ import { View, Text, StyleSheet } from 'react-native'
 
 const BoxScreen = () => {
     return (
-        <View style={styles.viewStyle}>
-            <Text style={styles.textStyle}>Child # 1</Text>
-            <Text style={styles.textStyle}>Child # 2</Text>
-            <Text style={styles.textStyle}>Child # 3</Text>
+        <View style={styles.parentStyle}>
+            <View style={styles.viewOneStyle} />
+            <View style={styles.viewTwoStyle} />
+            <View style={styles.viewThreeStyle} />
+            {/* <Text style={styles.textOneStyle}>Child # 1</Text>
+            <Text style={styles.textTwoStyle}>Child # 2</Text>
+            <Text style={styles.textThreeStyle}>Child # 3</Text> */}
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    viewStyle: {
+    parentStyle: {
         borderWidth: 3,
         borderColor: 'black',
-        alignItems: 'center'
+        height: 100,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
     },
-    textStyle: {
-        borderWidth: 3,
-        borderColor: 'red',
-    }
+    viewOneStyle: {
+        height: 50,
+        width: 50,
+        backgroundColor: 'red',
+    },
+    viewTwoStyle: {
+        height: 50,
+        width: 50,
+        backgroundColor: 'green',
+        alignSelf: 'flex-end'
+    },
+    viewThreeStyle: {
+        height: 50,
+        width: 50,
+        backgroundColor: 'purple',
+    },
+    // textOneStyle: {
+    //     borderWidth: 3,
+    //     borderColor: 'red',
+    // },
+    // textTwoStyle: {
+    //     borderWidth: 3,
+    //     borderColor: 'red',
+    //     flex: 1,
+    //     fontSize: 18,
+    //     ...StyleSheet.absoluteFillObject
+    // },
+    // textThreeStyle: {
+    //     borderWidth: 3,
+    //     borderColor: 'red',
+    // },
 })
 
 export default BoxScreen
